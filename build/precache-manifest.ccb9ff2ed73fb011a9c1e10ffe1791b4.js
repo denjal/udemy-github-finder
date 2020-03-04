a2f40364665deb23f9e00f143f54443b939549c4
Daniel Andersson <daniel.andersson.1991@gmail.com>
2020-03-04 11:07:59 +0100

    added routes

---
+++ build/precache-manifest.ccb9ff2ed73fb011a9c1e10ffe1791b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c7bfa52fbd1b33a982f70db1f31193f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1bdbe2b7eeadbc15a52c",
+    "url": "/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "afb3d3382e1f158874f3",
+    "url": "/static/js/2.d9d0cf8c.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.d9d0cf8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bdbe2b7eeadbc15a52c",
+    "url": "/static/js/main.1b05de22.chunk.js"
+  },
+  {
+    "revision": "169d1677017ef5180668",
+    "url": "/static/js/runtime-main.90411cac.js"
+  }
+]);
